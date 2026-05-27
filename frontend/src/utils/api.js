e8b978a2f336ec4395cd516e1975d12063a58c71
Dewang Mishra <mishradewang129@gmail.com>
2026-05-27 19:41:27 +0530

    fixed api url

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://cyberhub-backend-wtsb.onrender.com',
+  baseURL: 'https://cyberhub-backend-wtsb.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
